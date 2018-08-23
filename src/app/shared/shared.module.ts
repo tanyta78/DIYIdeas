21a1845ced468f73abf3f7c7152d6cf59802bb1f
Tatyana Milanova <tanyta78@gmail.com>
2018-08-23 11:41:06 +0300

    add custom filter pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { DropdownDirective } from "./dropdown.directive";
 import { CommonModule } from "@angular/common";
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
 	declarations:[
-		DropdownDirective
+		DropdownDirective,
+		FilterPipe
 	],
 	exports:[
 		CommonModule,
